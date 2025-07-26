08e378dbf2f3d48ef9419d4fa4c571c601f8b902
irfan-ahmed <irfan.ahmed@tallykhata.com>
2025-07-27 00:29:21 +0600

    Task update test [DONE]

--- tests/test_task_routes_update_task.py
+++ tests/test_task_routes_update_task.py
@@ -39,7 +39,7 @@ class TestTaskUpdate:
 
         client.post(self._task_url, headers=auth_headers, json=payload)
 
-    def _test_update_task(self, client):
+    def test_update_task(self, client):
         """
         test to update task
         :param client:
@@ -49,9 +49,65 @@ class TestTaskUpdate:
         self._create_task(client, auth_headers)
 
         payload = {
-            "is_complete": True
+            "title": "Test Task take 2",
+            "description": "Test Description",
+            "is_complete": True,
+            "due_date": "2025-07-28"
         }
-        response = client.put(self._task_url, headers=auth_headers, json=payload)
+        response = client.put(self._task_url + '/1', headers=auth_headers, json=payload)
 
-        assert response.status == status.HTTP_200_OK
+        assert response.status_code == status.HTTP_200_OK
         assert "updated successfully" in response.json().get('message')
+
+    def test_update_task_partial(self, client):
+        """
+        test to update task
+        :param client:
+        :return:
+        """
+        auth_headers = self._auth_headers(client)
+        self._create_task(client, auth_headers)
+
+        payload = {
+            "is_complete": True,
+            "due_date": "2025-07-28"
+        }
+        response = client.put(self._task_url + '/1', headers=auth_headers, json=payload)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert "updated successfully" in response.json().get('message')
+
+    def test_update_task_notfound(self, client):
+        """
+        test to update task
+        :param client:
+        :return:
+        """
+        auth_headers = self._auth_headers(client)
+
+        payload = {
+            "is_complete": True,
+            "due_date": "2025-07-28"
+        }
+        response = client.put(self._task_url + '/1', headers=auth_headers, json=payload)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert "not found" in response.json().get('detail')
+
+    def test_update_task_unauthorized(self, client):
+        """
+        test to update task
+        :param client:
+        :return:
+        """
+        auth_headers = self._auth_headers(client)
+        self._create_task(client, auth_headers)
+
+        payload = {
+            "is_complete": True,
+            "due_date": "2025-07-28"
+        }
+        response = client.put(self._task_url + '/1', headers=None, json=payload)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert "Not authenticated" in response.json().get('detail')
