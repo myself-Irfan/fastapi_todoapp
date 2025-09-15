7ecab6955053d9e8279f0309b9309aba377d2d41
irfan <irfan@systemss-MacBook-Pro.local>
2025-09-15 21:59:14 +0600

    next steps

--- app/main.py
+++ app/main.py
@@ -44,10 +44,9 @@ app.include_router(user_view_router)
 app.include_router(task_api_router)
 app.include_router(task_view_router)
 
-# TODO: Need to finish
+# TODO: Need to add dockerfile and .yml then complete
 # TODO: crontab like job reminding user to do their taskapp
 # TODO: add CI/CD
 # TODO: rate limiting
 # TODO: Clean architecture -> https://www.youtube.com/watch?v=H9Blu0kWdZE
 # TODO: SQLAlchemy to SQLModel
-# TODO: use structlog -> bind user_id so it shows up in log
